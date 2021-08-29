900775f5a3d7e289637bf9ed83f207ff015775b2
Ritter Gustave <ritter.gustave@sureprep.com>
2021-08-28 20:08:12 -0700

    (gallery): adding pictures and code

--- src/components/PhotoGallery/PhotoGallery.tsx
+++ src/components/PhotoGallery/PhotoGallery.tsx
@@ -1,7 +1,11 @@
-import { Typography } from '@material-ui/core';
 import React from 'react'
+import { Typography } from '@material-ui/core';
 
 import TopLevelContainer from '../../containers/TopLevelContainer';
+import images from './imgPull';
+// @ts-ignore
+import ImageGallery from 'react-image-gallery';
+import "react-image-gallery/styles/css/image-gallery.css";
 
 import styles from './PhotoGallery.module.css';
 import commonStyles from '../../common/common.module.css';
@@ -22,6 +26,19 @@ const Photogallery = () => {
                         Here are a few photos of our work!
                     </Typography>
                 </div>
+            </section>
+            <section className={styles.photosContainer}>
+                <ImageGallery items={images} />
+                {/* <ImageList rowHeight={160} className={classes.imageList} cols={3}>
+                    {images.map(( image, idx ) => (
+                        <ImageListItem key={idx} cols={1}>
+                            <img src={image} alt="" />
+                        </ImageListItem>
+                    ))}
+                </ImageList> */}
+                {/* {images.map(image => (
+                    <img src={image} alt="" className={styles.imgSizedDown}/>
+                ))} */}
             </section>
         </TopLevelContainer>
     )
---
+++ src/components/PhotoGallery/imgPull.ts
@@ -0,0 +1,5 @@
+const images = [
+	// TODO: Fill with imgs
+]
+
+export default images;
